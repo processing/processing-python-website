e6f722ac84a6b9ca06697a9b183da2f6cf5d8d00
Raphaël de Courville <groupes.raphael@gmail.com>
2024-02-27 12:55:10 +0100

    Added logging for debug
    Searching for the location of libgluegen-rt.so

--- headless_build_script.sh
+++ headless_build_script.sh
@@ -9,6 +9,9 @@ bash -c "./fetch_processing_jar.sh $TARGET_DIR"
 # Change to the target directory
 cd $TARGET_DIR
 
+echo "Searching for the location of libgluegen-rt.so..."
+find / -name libgluegen-rt.so 2>/dev/null
+
 # Run the build command with xvfb-run
 echo "Running in a headless environment. Building with xvfb..."
 xvfb-run python generator.py build --all --images
